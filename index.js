7a94f41b416e864a06414128ec84431045bec3e3
Johnny Yeng <a2902793@gmail.com>
2023-04-27 20:14:55 +0800

    feat: switch port to 80

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
-app.listen(3000, () => {
+app.listen(80, () => {
     console.log("Listen on the port 3000...");
 });
